050ed629321f8d2f3dd6a73ac9ae7e34b977dfb2
JhersonCastro <142602493+JhersonCastro@users.noreply.github.com>
2023-11-05 12:15:30 -0500

    Update EmpleadoPorHoras.java

--- Parcial1/src/Empleados/EmpleadoPorHoras.java
+++ Parcial1/src/Empleados/EmpleadoPorHoras.java
@@ -1,31 +1,24 @@
 package Empleados;
-
 public class EmpleadoPorHoras extends Empleado{
     private int horasTrabajadas;
     private double tarifaPorHora;
-
     public EmpleadoPorHoras(String nombre, int horasTrabajadas, double tarifaPorHora) {
         super(nombre, 0);
         this.horasTrabajadas = horasTrabajadas;
         this.tarifaPorHora = Math.round(tarifaPorHora * 100.0) / 100.0;
     }
-
     public int getHorasTrabajadas() {
         return horasTrabajadas;
     }
-
     public void setHorasTrabajadas(int horasTrabajadas) {
         this.horasTrabajadas = horasTrabajadas;
     }
-
     public double getTarifaPorHora() {
         return tarifaPorHora;
     }
-
     public void setTarifaPorHora(double tarifaPorHora) {
         this.tarifaPorHora = tarifaPorHora;
-    }
-   
+    }   
     @Override
     public double calcularSalario() {
         return Math.round((tarifaPorHora*horasTrabajadas)*100)/100;
