3f297822df761c0a1a51b565c8ca342331d9d446
JhersonCastro <142602493+JhersonCastro@users.noreply.github.com>
2023-11-05 12:15:10 -0500

    Update EmpleadoAsalariado.java

--- Parcial1/src/Empleados/EmpleadoAsalariado.java
+++ Parcial1/src/Empleados/EmpleadoAsalariado.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Empleados;
-
-/**
- *
- * @author IngSis
- */
 public class EmpleadoAsalariado extends Empleado{
     private final double deducciones;
 
@@ -15,13 +6,10 @@ public class EmpleadoAsalariado extends Empleado{
         super(nombre, Math.round(salarioBase * 100.0) / 100.0);
         this.deducciones = Math.round(deducciones * 100.0) / 100.0;
     }
-   
-    
     @Override
     public double calcularSalario() {
         return Math.round((salarioBase - deducciones) * 100.0) / 100.0;
     }
-
     @Override
     public String toString() {
         return "Nombre del EmpleadoAsalariado: "+ nombre
